abcce65e62be937d70f4624c22e2b35843f57974
Vladislav Samoilenko <79825223592@yandex.ru>
2024-10-11 16:44:03 +0300

    changed logs to file

--- src/handlers/logs.py
+++ src/handlers/logs.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 import pandas as pd
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import FSInputFile, Message
 
 from src.db.crud import Database
 
@@ -13,14 +13,19 @@ router = Router()
 @router.message(Command("logs"))
 async def cmd_logs(message: Message):
     now = message.date + +timedelta(hours=3)
-    start_date = (now - timedelta(days=45)).strftime("%Y-%m-%d %H:%M:%S")
+    start_date = (now - timedelta(days=60)).strftime("%Y-%m-%d %H:%M:%S")
     end_date = now.strftime("%Y-%m-%d %H:%M:%S")
 
     with Database() as db:
         records = db.get_between_dates(start_date, end_date)
+
     if records:
         df = pd.DataFrame(records, columns=["Тип", "Дата и время", "TG ID", "TG Username", "Количество", "Ссылка"])
-        logs = df.to_string(index=False)
-        await message.answer(f"<b>Логи с {start_date} по {end_date}:</b>\n\n<pre>{logs}</pre>", parse_mode="HTML")
+
+        file_path = "logs.csv"
+        df.to_csv(file_path, index=False)
+
+        log_file = FSInputFile(file_path)
+        await message.answer_document(document=log_file, caption=f"Логи с {start_date} по {end_date}")
     else:
         await message.answer(f"Логи за период с {start_date} по {end_date} не найдены.")
